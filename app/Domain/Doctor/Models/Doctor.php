ff0b0453e53a4e1ae4d9b0a488c3af044c9e3e10
Diego Pereira <san_diego19@hotmail.com>
2023-08-04 14:26:11 -0300

    feat: doctor patient relationship with middle table

--- app/Domain/Doctor/Models/Doctor.php
+++ app/Domain/Doctor/Models/Doctor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Doctor\Models;
 
+use App\Domain\Patient\Models\Patient;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -29,4 +30,10 @@ class Doctor extends Model
     protected $primaryKey = self::PRIMARY_KEY;
 
     protected $table = self::TABLE_NAME;
+
+    public function patients()
+    {
+        return $this->belongsToMany(Patient::class, 'doctor_patient', 'doctor_id', 'patient_id');
+    }
+
 }
